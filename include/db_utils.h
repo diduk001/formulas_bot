aef05b0d38f5c0bdaa3ec2f93b4544395f7b8608
egoridze74 <evkorneenko@edu.hse.ru>
2024-06-08 12:31:55 +0300

    Created working db

--- include/db_utils.h
+++ include/db_utils.h
@@ -41,6 +41,49 @@ namespace db {
     void close_conn() {
         sqlite3_close(db_conn);
     }
+
+    void create_working_db() {
+        //Table user
+        sqlite3_stmt *statement;
+        int rc = sqlite3_prepare_v2(db_conn, "CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY, tg_id INTEGER)".c_str(), -1, &statement, 0);
+        if (rc != SQLITE_OK) {
+            sqlite3_close(db_conn);
+            throw std::runtime_error("Error while preparing statement. Error code = " + std::to_string(rc));
+        }
+        sqlite3_step(statement);
+
+        //Table group
+        sqlite3_stmt *statement;
+        int rc = sqlite3_prepare_v2(db_conn, "CREATE TABLE IF NOT EXISTS `group` (group_id INTEGER PRIMARY KEY,"
+                                             " group_name TEXT, tg_id INTEGER, owner_id INTEGER, FOREIGN KEY (owner_id) REFERENCES user (user_id))".c_str(), -1, &statement, 0);
+        if (rc != SQLITE_OK) {
+            sqlite3_close(db_conn);
+            throw std::runtime_error("Error while preparing statement. Error code = " + std::to_string(rc));
+        }
+        sqlite3_step(statement);
+
+        //Table subject
+        sqlite3_stmt *statement;
+        int rc = sqlite3_prepare_v2(db_conn, "CREATE TABLE IF NOT EXISTS subject (subject_id INTEGER PRIMARY KEY, subject_name TEXT, group_id INTEGER, "
+                                             "description TEXT, professor_name TEXT, professor_email TEXT, FOREIGN KEY (group_id) REFERENCES `group` (group_id))".c_str(), -1, &statement, 0);
+        if (rc != SQLITE_OK) {
+            sqlite3_close(db_conn);
+            throw std::runtime_error("Error while preparing statement. Error code = " + std::to_string(rc));
+        }
+        sqlite3_step(statement);
+
+        //Table element
+        int rc = sqlite3_prepare_v2(db_conn, "CREATE TABLE IF NOT EXISTS element (element_id INTEGER PRIMARY KEY, name TEXT, subject_id INTEGER, value REAL, "
+                                             "block INTEGER, description TEXT, FOREIGN KEY (subject_id) REFERENCES Subject (subject_id))".c_str(), -1, &statement, 0);
+        sqlite3_stmt *statement;
+        if (rc != SQLITE_OK) {
+            sqlite3_close(db_conn);
+            throw std::runtime_error("Error while preparing statement. Error code = " + std::to_string(rc));
+        }
+        sqlite3_step(statement);
+
+        sqlite3_finalize(statement);
+    }
 }
 
 #endif //FORMULAS_TMP_DB_UTILS_H
