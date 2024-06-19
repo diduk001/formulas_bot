62feb71bdc4e87c312abcffc647a1b086a1cc3ff
diduk001 <didurenkos@gmail.com>
2024-06-19 06:11:42 +0300

    dirty hack to fix headers

--- formatters.sh
+++ formatters.sh
@@ -1,5 +1,5 @@
 find . -not \( -path "./lib/*" -o -path "./cmake-build-debug/*" -o -path "./build/*" -o -path "./CMakeFiles/*" \) \( -iname "*.h" -o -iname "*.cpp" \) -print0 | \
   xargs clang-format -dry-run -i --Werror
 
-cpplint --exclude=lib/ --exclude=cmake-build-debug/ --exclude=build/ --exclude=CMakeFiles --filter=-legal/copyright,-build/include_subdir,-build/c++11 --recursive .
+cpplint --exclude=lib/ --exclude=cmake-build-debug/ --exclude=build/ --exclude=CMakeFiles --filter=-legal/copyright,-build/include_subdir,-build/c++11,-build/include_order --recursive .
 cppcheck --std=c++20 -i cmake-build-debug -i lib -i build -i CMakeFiles .
